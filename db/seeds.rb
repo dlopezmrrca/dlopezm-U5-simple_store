21b26397b399b33da4a06736a271f7ffe254612a
Daniel <dlopezmaldonado@rrc.ca>
2024-06-14 11:33:22 -0500

    reading csv file

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,13 +14,14 @@ require "csv"
 # Product.find_by(title: "Valid Product")
 # Product.destroy_by(title: "Valid Product")
 
-Category.create(category_name: "testing")
+# Category.create(category_name: "testing")
 
-676.times do
-  Product.create(
-    title: Faker::Commerce.product_name,
-    description: Faker::Lorem.paragraph,
-    price: Faker::Commerce.price(range: 0..100.0),
-    stock_quantity: Faker::Number.between(from: 1, to: 100)
-  )
-end
+# Faker
+# 676.times do
+#   Product.create(
+#     title: Faker::Commerce.product_name,
+#     description: Faker::Lorem.paragraph,
+#     price: Faker::Commerce.price(range: 0..100.0),
+#     stock_quantity: Faker::Number.between(from: 1, to: 100)
+#   )
+# end
